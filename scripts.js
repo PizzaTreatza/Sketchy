76985a0db0fd4b0f9b6418533d0c71b29dd02434
PizzaTreatza-Away <mitchelld58@gmail.com>
2024-02-05 11:07:01 -0900

    updates for remote

--- scripts.js
+++ scripts.js
@@ -1,16 +1,35 @@
-var divSq = document.createElement("div");
 var squares = 16**2;
+squares.style.margin = "0";
+squares.style.padding = "0";
+squares.style.color = "ivory";
 
-function setSquares() {
+
+var divSq = document.createElement("div");
+divSq.style.width = 
+
+function getSquares() {
     var numIn = parseInt(prompt("Choose a board size between 1 & 100",""), 10);
     if (isNaN(numIn)) { 
         alert("Please enter a number");
-        setSquares();
+        getSquares();
     }
     else if (numIn > 100 || numIn < 1) {
         alert("Please choose a number between 1 & 100")
-        setSquares();
+        getSquares();
     }
         return numIn;
 }
-console.log(setSquares());
+
+function createSquares(dimensions) {
+    let div1 = document.createElement("div");
+    div1.style.width = '${dimensions}%';
+    div1.style.height = '${dimensions}%';
+
+    document.getElementById("board").appendChild(div1);
+}
+
+function setSquares () {
+    for (let i = 0; i < squares; i++) {
+        createSquares(squares);
+    }
+}
